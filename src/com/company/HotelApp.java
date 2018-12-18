04a8a330a54c80eb662405d1a9dcbc1f41aae610
Julius Soutine <jsoutine@gmail.com>
2018-12-18 08:27:25 +0100

    prepare for pull

--- src/com/company/HotelApp.java
+++ src/com/company/HotelApp.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 public class HotelApp {
     private Scanner input = new Scanner(System.in);
 
-
     public static void main(String[] args) {
 
         HotelApp myApp = new HotelApp();
